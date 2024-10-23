e9fba66f89ee71f7713498adaf2557ac69f90db1
RedDeadth <mimamamemima335@gmail.com>
2024-10-22 20:14:06 -0500

    Revert "perfomance"
    This reverts commit 651c92edaf3489aebd1f009191ad123afb8fc55b.

--- shield/usuarios/urls.py
+++ shield/usuarios/urls.py
@@ -2,15 +2,12 @@
 
 from django.urls import path
 from . import views
-from .views import verificar_token
 
 urlpatterns = [
     path('registrar/', views.registrar, name='registrar'),
     path('login/', views.login, name='login'),
     path('index/', views.index, name='index'),  # Ruta hacia la página de índice
     path('casilleros/', views.casilleros, name='casilleros'),  # Nueva ruta para casilleros.html
-    
+    path('detalle/<str:nombre>/<str:ubicacion>/<str:precio>/<str:descripcion>/', views.detalle, name='detalle'),
     path('logout/', views.logout, name='logout'),
-
-    path('verificar_token/', verificar_token, name='verificar_token'),
 ]
